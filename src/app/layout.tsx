7f1254b089c6381e3ae09612f8715c1b31576523
Jessica Dong <jessicad.7373@gmail.com>
2023-12-18 20:44:37 -0500

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next'
 import '../../styles/styles.css'
 
@@ -15,7 +16,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <link rel="icon" href="/icon.ico" sizes="any" />
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
