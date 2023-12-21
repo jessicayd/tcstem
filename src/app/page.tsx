84630b86c2b5e95d5265d518cb1f8247c2f147d1
serenahwang12 <106443799+serenahwang12@users.noreply.github.com>
2023-12-21 00:36:34 -0500

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from '../../components/Frame/Navbar'
 import Socials from '../../components/Frame/Socials'
 import Footer from '../../components/Frame/Footer'
 import ColorWrapper from '../../components/Wrapper/ColorWrapper'
+import Link from "next/link"
 
 export default function Home() {
   return (
@@ -9,7 +10,42 @@ export default function Home() {
       <Navbar />
       <div className="navbar-padding"></div>
       <ColorWrapper blue={false}>
-        <div>homeee</div>
+        <div className="home-banner">
+          <div className="home-banner-text">
+            <h1>Developing Future Thinkers</h1>
+            <div>Volunteer to impact students in STEM</div>
+            <div>Founded in NYC 2019</div>
+            <Link target="_blank" href="https://discord.com/invite/yCBHy2r7pb" className="default-button">Join our Discord Server</Link>
+          </div>
+          <div className="home-banner-img">
+            <img src="logo.png" alt="tcstem logo" width="300"/>
+          </div>
+        </div>
+      </ColorWrapper>
+      <ColorWrapper blue={true}>
+        <div className="home-impact">
+          <div className="home-years-impact">
+            <h1>5</h1>
+            <div>years of impact</div>
+          </div>
+          <div className="home-students-impact">
+            <h1>161</h1>
+            <div>students impacted</div>
+          </div>
+        </div>
+      </ColorWrapper>
+      <ColorWrapper blue={false}>
+        <div className="home-mission">
+          <h2>Our Mission</h2>
+          <div>teach. share. empower. </div>
+
+        </div>
+      </ColorWrapper>
+      <ColorWrapper blue={true}>
+        <h2>Testimonials</h2>
+      </ColorWrapper>
+      <ColorWrapper blue={false}>
+        <h2>Featured in</h2>
       </ColorWrapper>
       <ColorWrapper blue={true}><Socials/></ColorWrapper>
       <ColorWrapper blue={false}><Footer/></ColorWrapper>
